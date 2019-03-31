8d3c6391b9d7bb5ddf162075886c07a56a6f0ddc
daniel.logerstedt@gmail.com <daniel.logerstedt@gmail.com>
2019-03-31 14:17:50 -0700

    Initial Commit

---
+++ LINQApp/LINQApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LINQApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
